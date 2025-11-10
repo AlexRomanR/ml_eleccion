a73f3a061e634a43d295fa9671c835aab4c6b807
AlexRomanR <thealex6969@gmail.com>
2025-11-10 14:41:39 -0400

    nube

--- db.py
+++ db.py
@@ -6,6 +6,6 @@ def get_engine():
     # Usa variable de entorno DB_URL si existe; si no, usa tu BD local
     url = os.getenv(
         "DB_URL",
-        "mysql+pymysql://root:@localhost:3306/sw2_examen?charset=utf8mb4"
+        "mysql+pymysql://<USER>:<PASS>@<PUBLIC_HOST>:<PORT>/<DB>?charset=utf8mb4"
     )
     return create_engine(url, pool_pre_ping=True)
