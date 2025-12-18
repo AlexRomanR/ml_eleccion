5460b6ac6319d7eefd7662fdee4ccd69afbe720e
AlexRomanR <thealex6969@gmail.com>
2025-12-17 22:27:56 -0400

    cambios para subir a render

--- app/main.py
+++ app/main.py
@@ -1,14 +1,17 @@
+# app/main.py
 from fastapi import FastAPI
 from strawberry.fastapi import GraphQLRouter
 from app.api.schema import schema
 
 app = FastAPI()
 
-# GraphQL Router - Único punto de entrada
+@app.get("/health")
+def health():
+    return {"ok": True}
+
 graphql_app = GraphQLRouter(schema)
 app.include_router(graphql_app, prefix="/graphql")
 
-# Redirigir root a graphql para facilidad (opcional pero útil)
 @app.get("/")
 def root():
     return {"message": "Use /graphql endpoint"}
